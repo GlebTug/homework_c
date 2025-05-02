2c30f42aa306ef69b222940be6e87eadb5cc2af2
Gleb <tugarev02@inbox.ru>
2025-05-02 22:18:16 +0300

    first commit

---
+++ 1/без имени.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hello world!\n");
+	
+	printf("Let's\n");
+	printf("   go\n");
+	printf("     to walk\n");
+		
+	printf("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HHOHH\n");
+	printf(" ZZZZZ\n");
+	
+	return 0;
+}
+
